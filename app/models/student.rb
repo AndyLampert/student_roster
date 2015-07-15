1d7b33568fd9ea16e44d4413c7035e8db9953761
Andy Lampert <lamperta@gmail.com>
2015-07-15 17:46:02 -0600

    add in class work and validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,4 @@
 class Student < ActiveRecord::Base
+  validates :home_phone, :work_phone, :cell_phone, { presence: true, length: { is: 9 } }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150711052828) do
+ActiveRecord::Schema.define(version: 20150712015801) do
+
+  create_table "people", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "address_line_one"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip_code"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
